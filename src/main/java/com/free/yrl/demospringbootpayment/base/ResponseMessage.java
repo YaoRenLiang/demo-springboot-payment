c52166074d063fd6e7d3fd172e2283d6075a48c4
yaorenliang <yaorenliang@foxmail.com>
2020-06-29 13:43:51 +0800

    格式化

--- src/main/java/com/free/yrl/demospringbootpayment/base/ResponseMessage.java
+++ src/main/java/com/free/yrl/demospringbootpayment/base/ResponseMessage.java
@@ -5,6 +5,7 @@ import com.free.yrl.demospringbootpayment.constant.ResponseMessageConstants;
 import com.google.common.collect.Maps;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.io.Serializable;
@@ -22,125 +23,126 @@ import static org.springframework.util.ObjectUtils.isEmpty;
 @ApiModel(value = "response", description = "响应结果")
 public class ResponseMessage<T> implements Serializable {
 
-	@ApiModelProperty(value = "状态码", required = true)
-	private Integer code;
+    @ApiModelProperty(value = "状态码", required = true)
+    private Integer code;
 
-	@ApiModelProperty("调用结果消息")
-	private String message;
+    @ApiModelProperty("调用结果消息")
+    private String message;
 
-	@ApiModelProperty(value = "响应数据")
-	private final T data;
+    @ApiModelProperty(value = "响应数据")
+    private final T data;
 
-	private ResponseMessage(Integer code, String message, T data) {
-		this.code = code;
-		this.message = message;
-		this.data = data;
-		if (isEmpty(code)) {
-			this.code = ResponseMessageConstants.SERVICE_EXCEPTION.getKey();
-		}
-		if (isEmpty(message)) {
-			this.message = ResponseMessageConstants.SERVICE_EXCEPTION.getValue();
-		}
-	}
+    private ResponseMessage(Integer code, String message, T data) {
 
-	/**
-	 * 根据状态码返回
-	 *
-	 * @param resultCode 结果状态码
-	 * @return 状态码
-	 */
-	public static <T> ResponseMessage<T> byCode(Integer resultCode) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+        if (isEmpty(code)) {
+            this.code = ResponseMessageConstants.SERVICE_EXCEPTION.getKey();
+        }
+        if (isEmpty(message)) {
+            this.message = ResponseMessageConstants.getValueByKey(getCode());
+        }
 
-		if (!resultCode.equals(ResponseMessageConstants.SUCCESS_OPERATION.getKey())) {
-			return error(resultCode);
-		}
-		return success();
+    }
 
-	}
+    /**
+     * 根据状态码返回
+     *
+     * @param resultCode 结果状态码
+     * @return 状态码
+     */
+    public static <T> ResponseMessage<T> byCode(Integer resultCode) {
 
-	/**
-	 * 根据数据处理成功的条数来返回
-	 *
-	 * @param successRow 成功的行数（例如：c u d操作）
-	 * @return 状态码
-	 */
-	public static Integer byRow(int successRow) {
+        if (!resultCode.equals(ResponseMessageConstants.SUCCESS_OPERATION.getKey())) {
+            return error(resultCode);
+        }
+        return success();
 
-		if (successRow <= 0) {
-			return ResponseMessageConstants.SERVICE_EXCEPTION.getKey();
-		}
-		return ResponseMessageConstants.SUCCESS_OPERATION.getKey();
+    }
 
-	}
+    /**
+     * 根据数据处理成功的条数来返回
+     *
+     * @param successRow 成功的行数（例如：c u d操作）
+     * @return 状态码
+     */
+    public static Integer byRow(int successRow) {
 
-	/**
-	 * 返回错误
-	 *
-	 * @param constants 枚举
-	 * @param <T>       范性
-	 * @return 返回体
-	 */
-	public static <T> ResponseMessage<T> error(ResponseMessageConstants constants) {
+        if (successRow <= 0) {
+            return ResponseMessageConstants.SERVICE_EXCEPTION.getKey();
+        }
+        return ResponseMessageConstants.SUCCESS_OPERATION.getKey();
 
-		Integer key = constants.getKey();
-		return error(key);
+    }
 
-	}
+    /**
+     * 返回错误
+     *
+     * @param constants 枚举
+     * @param <T>       范性
+     * @return 返回体
+     */
+    public static <T> ResponseMessage<T> error(ResponseMessageConstants constants) {
 
-	/**
-	 * 返回错误
-	 *
-	 * @param code 状态码
-	 * @param <T>  范性
-	 * @return 返回体
-	 */
-	public static <T> ResponseMessage<T>
-	error(Integer code) {
+        Integer key = constants.getKey();
+        return error(key);
 
-		return new ResponseMessage<>(code, msg(code), null);
+    }
 
-	}
+    /**
+     * 返回错误
+     *
+     * @param code 状态码
+     * @param <T>  范性
+     * @return 返回体
+     */
+    public static <T> ResponseMessage<T> error(Integer code) {
 
-	/**
-	 * 返回成功
-	 *
-	 * @param <T> 范性
-	 * @return 返回体
-	 */
-	public static <T> ResponseMessage<T> success() {
+        return new ResponseMessage<>(code, msg(code), null);
 
-		Integer key = ResponseMessageConstants.SUCCESS_OPERATION.getKey();
-		return new ResponseMessage<>(key, msg(key), null);
+    }
 
-	}
+    /**
+     * 返回成功
+     *
+     * @param <T> 范性
+     * @return 返回体
+     */
+    public static <T> ResponseMessage<T> success() {
 
-	/**
-	 * 返回成功
-	 *
-	 * @param data 需要返回的数据
-	 * @param <T>  范性
-	 * @return 返回体
-	 */
-	public static <T> ResponseMessage<T> success(Object data) {
+        Integer key = ResponseMessageConstants.SUCCESS_OPERATION.getKey();
+        return new ResponseMessage<>(key, msg(key), null);
 
-		Integer key = ResponseMessageConstants.SUCCESS_OPERATION.getKey();
-		if (data instanceof String || data instanceof Integer || data instanceof Date) {
-			Map<String, Object> map = Maps.newHashMap();
-			map.put("result", data);
-			return new ResponseMessage<>(key, msg(key), (T) map);
-		}
-		String a = new String("");
-		if (isEmpty(data)) {
-			data = null;
-		}
-		return new ResponseMessage<>(key, msg(key), (T) data);
+    }
 
-	}
+    /**
+     * 返回成功
+     *
+     * @param data 需要返回的数据
+     * @param <T>  范性
+     * @return 返回体
+     */
+    public static <T> ResponseMessage<T> success(Object data) {
 
-	private static String msg(Integer key, Object... params) {
+        Integer key = ResponseMessageConstants.SUCCESS_OPERATION.getKey();
+        if (data instanceof String || data instanceof Integer || data instanceof Date) {
+            Map<String, Object> map = Maps.newHashMap();
+            map.put("result", data);
+            return new ResponseMessage<>(key, msg(key), (T) map);
+        }
+        String a = new String("");
+        if (isEmpty(data)) {
+            data = null;
+        }
+        return new ResponseMessage<>(key, msg(key), (T) data);
 
-		return Resources.getMessage("CODE_" + key, params);
+    }
 
-	}
+    private static String msg(Integer key, Object... params) {
+
+        return Resources.getMessage("CODE_" + key, params);
+
+    }
 
 }
--- src/main/java/com/free/yrl/demospringbootpayment/constant/ResponseMessageConstants.java
+++ src/main/java/com/free/yrl/demospringbootpayment/constant/ResponseMessageConstants.java
@@ -1,6 +1,8 @@
 package com.free.yrl.demospringbootpayment.constant;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -10,36 +12,36 @@ import java.io.Serializable;
  * @author 姚壬亮
  **/
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public enum ResponseMessageConstants implements Serializable {
 
 	SUCCESS_OPERATION(0, "操作成功"),
 	SERVICE_EXCEPTION(-100, "服务异常");
 
-
 	private Integer key;
 	private String value;
 
-	ResponseMessageConstants(Integer key, String value) {
-		this.key = key;
-		this.value = value;
-	}
-
 	public static String getValueByKey(Integer key) {
+
 		for (ResponseMessageConstants gender : ResponseMessageConstants.values()) {
 			if (gender.getKey().equals(key)) {
 				return gender.getValue();
 			}
 		}
 		return "";
+
 	}
 
 	public static Integer getKeyByValue(Integer value) {
+
 		for (ResponseMessageConstants gender : ResponseMessageConstants.values()) {
 			if (value.equals(gender.getValue())) {
 				return gender.getKey();
 			}
 		}
 		return null;
+
 	}
 
 }
